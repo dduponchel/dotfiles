ebeaeeed78b6e78722fec09017918eb9bdbe5b0c
David Duponchel <d.duponchel@gmail.com>
2010-09-21 23:53:22 +0200

    corrected strange behavior with no X server

--- zsh/50_termsupport.zsh
+++ zsh/50_termsupport.zsh
@@ -1,6 +1,9 @@
-preexec () {
-	print -Pn "\e]0;%n@%m: $1\a"
-}
-precmd () {
-	print -Pn "\e]0;%n@%m: %~\a"
-}
+if [ -n "$DISPLAY" ]
+then
+  preexec () {
+    print -Pn "\e]0;%n@%m: $1\a"
+  }
+  precmd () {
+    print -Pn "\e]0;%n@%m: %~\a"
+  }
+fi
