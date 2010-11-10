2803ce2b0cd46e6c17b3b99116248912eb15c20a
David Duponchel <d.duponchel@gmail.com>
2010-11-10 19:46:48 +0100

    updated .symlink_it.sh : can create links in any folder

--- .symlink_it.sh
+++ .symlink_it.sh
@@ -4,12 +4,15 @@
 cd `dirname $0`
 DOTFILES=$PWD
 
+DESTDIR=${1:-~}
+[ ! -d $DESTDIR ] && echo "$DESTDIR is not a folder" 1>&2 && exit 1
+
 # relink config_file [dest]
 # If dest is not specified, dest = ~/.${config_file}
 # remove dest if possible and create a symlink from config_file to dest.
 function relink() {
   config_file=$DOTFILES/$1
-  dest=${2:-~/.$1}
+  dest=${2:-$DESTDIR/.$1}
 
   [ -e $dest ] && rm -ri $dest
   ln -sn $config_file $dest
@@ -21,21 +24,21 @@ relink conkystatusbarrc
 relink dzen
 relink mplayer
 relink vim
-relink vim-pathogen/autoload ~/.vim/autoload
+relink vim-pathogen/autoload $DESTDIR/.vim/autoload
 relink vimrc
 relink xinitrc
 relink Xresources
 relink zsh
-relink profile ~/.zprofile
+relink profile $DESTDIR/.zprofile
 # for bash compatibility, and xinitrc (see comments)
-relink profile ~/.bash_profile
+relink profile $DESTDIR/.bash_profile
 relink bashrc
 relink zshrc
 
-mkdir -p .xmonad
-relink xmonad.hs ~/.xmonad/xmonad.hs
+mkdir -p $DESTDIR/.xmonad
+relink xmonad.hs $DESTDIR/.xmonad/xmonad.hs
 
 # quodlibet plugins : not the ideal way to handle my changes...
-mkdir -p .quodlibet/plugins/{editing,songsmenu}/
+mkdir -p $DESTDIR/.quodlibet/plugins/{editing,songsmenu}/
 relink quodlibet/plugins/editing/iconv.py
 relink quodlibet/plugins/songsmenu/openwith.py
