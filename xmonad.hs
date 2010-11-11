4a72a8749283ac12c2a1b5d40cffff3e6dd2865f
David Duponchel <d.duponchel@gmail.com>
2010-11-11 14:45:34 +0100

    updated layout in xmonad

--- xmonad.hs
+++ xmonad.hs
@@ -21,6 +21,9 @@ import XMonad.Util.EZConfig(additionalKeys)
 import Data.Ratio ((%))
 import XMonad.Layout.ResizableTile
 import XMonad.Layout.NoBorders
+import XMonad.Layout.Tabbed
+import XMonad.Layout.Grid
+
 import XMonad.Hooks.ManageHelpers
 
 import qualified XMonad.StackSet as W
@@ -103,6 +106,28 @@ myKeys conf@(XConfig {XMonad.modMask = modm}) =
 myLogHook = dynamicLogWithPP dzenPP
 
 ------------------------------------------------------------------------
+-- Layouts:
+
+-- You can specify and transform your layouts by modifying these values.
+-- If you change layout bindings be sure to use 'mod-shift-space' after
+-- restarting (with 'mod-q') to reset your layout state to the new
+-- defaults, as xmonad preserves your old layout settings by default.
+--
+-- The available layouts.  Note that each layout is separated by |||,
+-- which denotes layout choice.
+--
+myLayout = Grid ||| tiled ||| Mirror tiled ||| simpleTabbed ||| Full
+  where
+     -- default tiling algorithm partitions the screen into two panes
+     tiled   = Tall nmaster delta ratio
+     -- The default number of windows in the master pane
+     nmaster = 1
+     -- Default proportion of screen occupied by master pane
+     ratio   = 1/2
+     -- Percent of screen to increment by when resizing panes
+     delta   = 3/100
+
+------------------------------------------------------------------------
 -- Now run xmonad with all the defaults we set up.
 
 -- Run xmonad with the settings you specify. No need to modify this.
@@ -116,7 +141,7 @@ main = do
         , focusedBorderColor="#990000"
         , borderWidth = 1
         , startupHook = myStartupHook
-        , layoutHook = smartBorders (avoidStruts  $  layoutHook defaultConfig)
+        , layoutHook = smartBorders (avoidStruts $ myLayout)
         , workspaces = myWorkspaces
         , keys = newKeys
         , logHook = myLogHook
