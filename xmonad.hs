d9451b8c4ee0443dc6978172f26fc546c70eaa2a
David Duponchel <d.duponchel@gmail.com>
2010-11-18 00:00:57 +0100

    simplifying xmonad workspaces

--- xmonad.hs
+++ xmonad.hs
@@ -50,7 +50,7 @@ myModMask = mod4Mask
 --
 -- > workspaces = ["web", "irc", "code" ] ++ map show [4..9]
 --
-myWorkspaces = ["1","2","3","4","5","6","7","8","9"]
+myWorkspaces = map show [1..9]
 
 ------------------------------------------------------------------------
 -- Window rules:
