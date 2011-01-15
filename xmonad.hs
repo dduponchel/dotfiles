1574824b6eab860a84a2706bb1f1996fd482e84b
David Duponchel <d.duponchel@gmail.com>
2011-01-15 03:07:04 +0100

    urgencyhook, websearch and GridSelect

--- xmonad.hs
+++ xmonad.hs
@@ -6,30 +6,33 @@
 --
 -- Normally, you'd only override those defaults you care about.
 --
-
 import Data.Monoid
 import Data.Ratio ((%))
 import System.Exit
 import System.IO
 import XMonad
-import XMonad.Actions.CycleWS
+import XMonad.Actions.GridSelect
 import XMonad.Actions.Plane
+import XMonad.Actions.Search
 import XMonad.Hooks.DynamicLog
-import XMonad.Hooks.FadeInactive
 import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.ManageHelpers
 import XMonad.Hooks.SetWMName
+import XMonad.Hooks.UrgencyHook
 import XMonad.Layout.Grid
 import XMonad.Layout.NoBorders
 import XMonad.Layout.ResizableTile
 import XMonad.Layout.Tabbed
 import XMonad.Prompt
+import XMonad.Prompt.Shell
 import XMonad.Prompt.Workspace
 import XMonad.Util.EZConfig(additionalKeys)
 import XMonad.Util.Run(spawnPipe)
-
-import qualified XMonad.StackSet as W
-import qualified Data.Map        as M
+import qualified Data.Map              as M
+import qualified XMonad.Actions.Search as S
+import qualified XMonad.Actions.Submap as SM
+import qualified XMonad.Prompt         as P
+import qualified XMonad.StackSet       as W
 
 -- The preferred terminal program, which is used in a binding below and by
 -- certain contrib modules.
@@ -93,6 +96,28 @@ myManageHook = composeAll
 -- http://bbs.archlinux.org/viewtopic.php?pid=744649
 myStartupHook = setWMName "LG3D"
 
+
+myXPConfig = defaultXPConfig
+    { fgColor  = "#55ff99"
+    , bgColor  = "#000000"
+    , bgHLight = "#000000"
+    , fgHLight = "#FF0000"
+    , position = Top
+    }
+
+--Search engines to be selected
+--keybinding: hit mod + s + <searchengine>
+searchEngineMap method = M.fromList $
+       [ ((0, xK_g), method S.google )
+       , ((0, xK_y), method S.youtube )
+       , ((0, xK_m), method S.maps )
+       , ((0, xK_w), method S.wikipedia )
+       , ((0, xK_b), method $ S.searchEngine "archbbs" "http://bbs.archlinux.org/search.php?action=search&keywords=")
+       , ((0, xK_r), method $ S.searchEngine "AUR" "http://aur.archlinux.org/packages.php?O=0&L=0&C=0&K=")
+       , ((0, xK_a), method $ S.searchEngine "archwiki" "http://wiki.archlinux.org/index.php/Special:Search?search=")
+       , ((0, xK_d), method $ S.searchEngine "anidb" "http://anidb.net/perl-bin/animedb.pl?show=animelist&adb.search=")
+       ]
+
 ------------------------------------------------------------------------
 -- Key bindings. Add, modify or remove key bindings here.
 --
@@ -108,7 +133,11 @@ myKeys conf@(XConfig {XMonad.modMask = modMask}) =
     , ((modMask, xK_quoteleft),        spawn "rotatexkbmap") -- with qwerty keyboard
     , ((modMask, xK_twosuperior),      spawn "rotatexkbmap") -- with azerty keyboard
     -- go to a named workspace
-    , ((modMask, xK_g),                workspacePrompt defaultXPConfig (windows . W.shift))
+    , ((modMask, xK_g),                workspacePrompt myXPConfig (windows . W.shift))
+    , ((modMask .|. shiftMask, xK_g),  goToSelected defaultGSConfig)
+    , ((modMask, xK_s ),               SM.submap $ searchEngineMap $ S.promptSearchBrowser myXPConfig "firefox")
+    , ((modMask .|. shiftMask, xK_p), shellPrompt myXPConfig)
+    , ((modMask, xK_b ), sendMessage ToggleStruts)
     ]
     ++
     -- Switch workspaces (and move windows) vertically
@@ -152,16 +181,17 @@ myLayout = Grid ||| tiled ||| Mirror tiled ||| simpleTabbed ||| Full
 ------------------------------------------------------------------------
 -- Now run xmonad with all the defaults we set up.
 
+
 -- Run xmonad with the settings you specify. No need to modify this.
 --
 main = do
-    xmonad $ defaultConfig {
+    xmonad $ withUrgencyHook NoUrgencyHook $ defaultConfig {
         manageHook = manageDocks <+> myManageHook <+> manageHook defaultConfig
         , terminal = myTerminal
         , modMask = myModMask
-        , normalBorderColor="#000044"
-        , focusedBorderColor="#990000"
-        , borderWidth = 1
+        , normalBorderColor="#000000"
+        , focusedBorderColor="#009900"
+        , borderWidth = 2
         , startupHook = myStartupHook
         , layoutHook = smartBorders (avoidStruts $ myLayout)
         , workspaces = myWorkspaces
