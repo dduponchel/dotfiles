1f3a1d97eb766e0c5568831ce10a04a1756b9273
David Duponchel <d.duponchel@gmail.com>
2011-01-16 13:47:15 +0100

    display time stats if a process took too long

--- zsh/50_misc.zsh
+++ zsh/50_misc.zsh
@@ -5,6 +5,10 @@ zle -N self-insert url-quote-magic
 # List jobs in the long format by default.
 setopt long_list_jobs
 
+# if user and system execution times > Xs, display timing stats
+REPORTTIME=10
+# use this format to report execution times
+TIMEFMT="$bold_color%J$reset_color took ${fg[red]}%E$reset_color. ${fg[yellow]}%U$reset_color user, ${fg[yellow]}%S$reset_color system, ${fg[yellow]}%P$reset_color cpu"
 
 alias notepad++='wine "C:\Program Files\Notepad++\notepad++.exe"'
 command_exists rlwrap && alias telnet="rlwrap telnet $@"
