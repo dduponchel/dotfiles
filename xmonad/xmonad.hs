83f16aa27609bf4cc6583968b433e887afec5108
David Duponchel <d.duponchel@gmail.com>
2011-01-19 00:28:14 +0100

    Removed useless / unused code. Removed functionalities : 20 workspaces on 2 lines, go to a named workspace, search engines.

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,11 +1,5 @@
-import Data.Monoid
-import Data.Ratio ((%))
-import System.Exit
-import System.IO
 import XMonad
 import XMonad.Actions.GridSelect
-import XMonad.Actions.Plane
-import XMonad.Actions.Search
 import XMonad.Actions.SpawnOn
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
@@ -15,18 +9,10 @@ import XMonad.Hooks.UrgencyHook
 import XMonad.Layout.Grid
 import XMonad.Layout.Named
 import XMonad.Layout.NoBorders
-import XMonad.Layout.ResizableTile
 import XMonad.Layout.Tabbed
 import XMonad.Prompt
-import XMonad.Prompt.Shell
-import XMonad.Prompt.Workspace
-import XMonad.Util.EZConfig(additionalKeys)
-import XMonad.Util.Run(spawnPipe)
-import qualified Data.Map              as M
-import qualified XMonad.Actions.Search as S
-import qualified XMonad.Actions.Submap as SM
-import qualified XMonad.Prompt         as P
-import qualified XMonad.StackSet       as W
+import qualified Data.Map        as M
+import qualified XMonad.StackSet as W
 
 {-
  - I have more than one pc with xmonad, each with different uses and different programs.
@@ -65,8 +51,8 @@ myTerminal = "urxvtc"
 myModMask = mod4Mask
 
 -- workspaces = ["web", "irc", "code" ] ++ map show [4..9]
--- myWorkspaces = map show [1..10]
-myWorkspaces = map concat (sequence [["u", "d"], (map show [1..10])])
+myWorkspaces = map show [1..10]
+-- myWorkspaces = map concat (sequence [["u", "d"], (map show [1..10])])
 
 -- Window rules
 -- To find the property name associated with a program, use
@@ -94,19 +80,6 @@ myXPConfig = defaultXPConfig
     , position = Top
     }
 
--- Search engines to be selected
--- keybinding: hit mod + s + <searchengine>
-searchEngineMap method = M.fromList $
-       [ ((0, xK_g), method S.google )
-       , ((0, xK_y), method S.youtube )
-       , ((0, xK_m), method S.maps )
-       , ((0, xK_w), method S.wikipedia )
-       , ((0, xK_b), method $ S.searchEngine "archbbs" "http://bbs.archlinux.org/search.php?action=search&keywords=")
-       , ((0, xK_r), method $ S.searchEngine "AUR" "http://aur.archlinux.org/packages.php?O=0&L=0&C=0&K=")
-       , ((0, xK_a), method $ S.searchEngine "archwiki" "http://wiki.archlinux.org/index.php/Special:Search?search=")
-       , ((0, xK_d), method $ S.searchEngine "anidb" "http://anidb.net/perl-bin/animedb.pl?show=animelist&adb.search=")
-       ]
-
 -- Key bindings.
 newKeys x = M.union (M.fromList (myKeys x)) (keys defaultConfig x)
 myKeys conf@(XConfig {XMonad.modMask = modMask}) =
@@ -119,34 +92,23 @@ myKeys conf@(XConfig {XMonad.modMask = modMask}) =
     , ((modMask, xK_Home),             spawn "quodlibet --play-pause")
     , ((modMask, xK_quoteleft),        spawn "rotatexkbmap") -- with qwerty keyboard
     , ((modMask, xK_twosuperior),      spawn "rotatexkbmap") -- with azerty keyboard
-    -- go to a named workspace
-    , ((modMask, xK_g),                workspacePrompt myXPConfig (windows . W.shift))
     -- go to a window
     , ((modMask .|. shiftMask, xK_g),  goToSelected defaultGSConfig)
-    -- search something, use the browser for the result
-    , ((modMask, xK_s ),               SM.submap $ searchEngineMap $ S.promptSearchBrowser myXPConfig "firefox")
-    -- like modMask + xK_p, but xmonad's shellprompt
-    , ((modMask .|. shiftMask, xK_p), shellPrompt myXPConfig)
     -- toggle the space for the statusbar
     , ((modMask, xK_b ), sendMessage ToggleStruts)
     ]
     ++
-    -- Switch workspaces (and move windows) vertically
-    -- don't ask me how the code works, I really need to read a haskell book
-    -- TODO : do I really need 20 workspaces ?
-    [((keyMask .|. modMask, keySym), function (Lines 2) Finite direction)
-     | (keySym, direction) <- zip [xK_Left .. xK_Down] $ enumFrom ToLeft
-     , (keyMask, function) <- [(0, planeMove), (shiftMask, planeShift)]
-    ]
-    ++
     -- same as /usr/share/xmonad-0.9.1/man/xmonad.hs, but add xK_0
     -- 10 workspaces !
+    -- same as adding
+    -- , ((modMask              , xK_0), windows $ W.greedyView (myWorkspaces!!9))
+    -- , ((modMask .|. shiftMask, xK_0), windows $ W.shift      (myWorkspaces!!9))
     [((m .|. modMask, k), windows $ f i)
         | (i, k) <- zip (XMonad.workspaces conf) ([xK_1 .. xK_9] ++ [xK_0])
         , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]
     ]
 
--- dynamicLog for dzen. use xmobarPP for xmobar.
+-- dynamicLog for dzen. use xmobarPP or dzenPP for nice defaults
 myLogHook = dynamicLogWithPP defaultPP
   { ppCurrent = dzenColor "#09F" "" -- wrap "[" "]"
   , ppVisible = dzenColor "white" "" -- wrap "<" ">"
