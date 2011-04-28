2747241107e539955a36ab80bb7820651c4fb6ab
David Duponchel <d.duponchel@gmail.com>
2011-04-28 19:26:30 +0200

    add some firefox prefs

--- .symlink_it.sh
+++ .symlink_it.sh
@@ -51,3 +51,11 @@ relink shell/zshrc
 # quodlibet plugins : not the ideal way to handle my changes...
 relink {,.}quodlibet/plugins/editing/iconv.py
 relink {,.}quodlibet/plugins/songsmenu/openwith.py
+
+# TODO find why bash doesn't expand "for profile in .mozilla/firefox/*/prefs.js"
+# with no match (should do 0 loop)
+for profile in `ls .mozilla/firefox/*/prefs.js 2> /dev/null`
+do
+  prefs=${profile%prefs.js}user.js
+  relink firefox/user.js $prefs
+done
---
+++ firefox/user.js
@@ -0,0 +1,8 @@
+// DNS resolution on the proxy when using a SOCKS proxy
+user_pref("network.proxy.socks_remote_dns", true);
+
+// open search results in a new tab
+user_pref("browser.search.openintab", true);
+
+// the middle click won't try to load an url
+user_pref("middlemouse.contentLoadURL", false);
