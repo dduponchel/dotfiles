b938ddaab807ec15d2662ef7b98b860f96cfbb0a
David Duponchel <d.duponchel@gmail.com>
2011-06-18 00:33:07 +0200

    misc functions

--- shell/zsh/50_misc.zsh
+++ shell/zsh/50_misc.zsh
@@ -16,3 +16,14 @@ command_exists rlwrap && alias telnet="rlwrap telnet $@"
 alias :q="exit"
 alias vim="vim -p"
 alias ss="sudo su -"
+
+
+function down4me() {
+  curl -s "http://www.downforeveryoneorjustme.com/$1" | sed '/just you/!d;s/<[^>]*>//g'
+}
+
+function getip {
+  ifconfig | grep "inet " | awk '{ print $2 }'
+  res=$(curl -s checkip.dyndns.org | grep -Eo '[0-9\.]+')
+  echo "Public IP : $(curl -s checkip.dyndns.org | grep -Eo '[0-9\.]+')"
+}
