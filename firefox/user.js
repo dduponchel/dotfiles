611caf7cb0cefb704d01784a42a368c1e095dedc
David Duponchel <d.duponchel@gmail.com>
2011-06-24 17:29:19 +0200

    firefox : use by default the DNT header

--- firefox/user.js
+++ firefox/user.js
@@ -6,3 +6,6 @@ user_pref("browser.search.openintab", true);
 
 // the middle click won't try to load an url
 user_pref("middlemouse.contentLoadURL", false);
+
+// DO NOT TRACK
+user_pref("privacy.donottrackheader.enabled", true);
